d0b4e64ec8c2b7980024ace466a966b59afad134
SJdesigns <s.alvarez2011@hotmail.com>
2017-11-27 17:20:43 +0100

    Add files via upload

---
+++ js/edit.js
@@ -0,0 +1,127 @@
+$(function() {
+	var settings = JSON.parse(localStorage.getItem('startz-settings'));
+	var items = JSON.parse(localStorage.getItem('startz-items'));
+
+	var engines = [
+		{'name':'google','search':'http://www.google.com/search','queryVar':'q'},
+		{'name':'bing','search':'https://www.bing.com/search','queryVar':'q'},
+		{'name':'yahoo','search':'https://es.search.yahoo.com/search','queryVar':'p'},
+		{'name':'DuckDuckGo','search':'https://duckduckgo.com','queryVar':'q'}
+	];
+
+	cont = 0;
+	box = '<table border="0"><thead><tr><th>Título</th><th>Logotipo</th><th>Enlace</th><th></th><th></th><th></th></tr></thead><tbody>';
+	for (i in items) {
+		if (items[i]['count'] == undefined) {
+			items[i]['count'] = 0;
+		}
+		box += '<tr class="tablerow">';
+			box += '<td><input class="title" type="text" name="title' + i + '" value="' + items[i]['tit'] + '" /></td>';
+			box += '<td><input class="logo" type="text" name="logo' + i + '" value="' + items[i]['logo'] + '" /></td>';
+			box += '<td><input class="link" type="text" name="link' + i + '" value="' + items[i]['link'] + '" /><input class="count" type="hidden" name="count" value="' + items[i]['count'] + '" /></td>';
+			box += '<td><svg class="upRow" fill="#333" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/><path d="M0 0h24v24H0z" fill="none"/></svg></td>';
+			box += '<td><svg class="downRow" fill="#333" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/><path d="M0-.75h24v24H0z" fill="none"/></svg></td>';
+			box += '<td><svg class="deleteRow" fill="#333" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg></td>';
+		box += '</tr>';
+		cont++;
+	}
+	box += '</tbody></table>';
+
+	$('#tablaCuadros').html(box);
+
+	// botón para añadir nuevos enlaces
+	$('#addLink').on('click',function() {
+		newRow = '<tr class="tablerow">';
+			newRow += '<td><input class="title" type="text" placeholder="nuevo título" /></td>';
+			newRow += '<td><input class="logo" type="text" placeholder="nuevo logotipo" /></td>';
+			newRow += '<td><input class="link" type="text" placeholder="nuevo enlace" /><input class="count" type="hidden" name="count" value="0" /></td>';
+			newRow += '<td><svg class="upRow" fill="#333" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/><path d="M0 0h24v24H0z" fill="none"/></svg></td>';
+			newRow += '<td><svg class="downRow" fill="#333" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/><path d="M0-.75h24v24H0z" fill="none"/></svg></td>';
+			newRow += '<td><svg class="deleteRow" fill="#333" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg></td>';
+		newRow += '</tr>';
+		$('#tablaCuadros').find('table').find('tbody').append(newRow);
+		boton();
+	});
+
+	$('#configUsername').val(settings.username);
+	$('#configAvatar').val(settings.avatar);
+	$('.searchEngine').find('select').val(settings.searchEngine).change();
+
+	for (i in engines) {
+		if (engines[i]['name'] == settings.searchEngine) {
+			$('header').find('form').attr('action',engines[i].search);
+			$('#searchBox').attr('name',engines[i].queryVar);
+			$('#searchBox').attr('placeholder','buscar con ' + engines[i].name);
+		}
+	}
+
+	// botón para guardar cambios
+	$('#saveButton').on('click',function() {
+		var title = Array();
+		var logo = Array();
+		var link = Array();
+		var count = Array();
+
+		$('.tablerow').each(function(e) {
+			title.push($(this).find('td').find('.title').val());
+			logo.push($(this).find('td').find('.logo').val());
+			link.push($(this).find('td').find('.link').val());
+			count.push($(this).find('td').find('.count').val());
+		});
+
+		var items = [];
+		for (i in title) {
+			if (title[i] != '') {
+				items.push({'tit':title[i],'logo':logo[i],'link':link[i],'count':count[i]});
+			}
+		}
+
+		var newSettings = {
+			'avatar': $('#configAvatar').val(),
+			'username': $('#configUsername').val(),
+			'searchEngine': $('.searchEngine').find('select').val()
+		};
+
+		localStorage.setItem('startz-settings',JSON.stringify(newSettings));
+		localStorage.setItem('startz-items',JSON.stringify(items));
+
+		window.location = '../index.html';
+	});
+
+	// activar los botones de desplazamiento y borrado de los items
+	boton();
+
+	function boton() {
+		$('.upRow').on('click',function(e) {
+			console.log('mover para arriba');
+			var mover = $(this).parent().parent();
+			console.log(mover);
+			console.log(mover.prev());
+
+			$(mover.prev()).fadeOut(200).before(mover).fadeIn(200);
+			$(mover).fadeOut(200).fadeIn(200);
+		});
+
+		$('.downRow').on('click',function(e) {
+			console.log('mover para abajo');
+			var mover = $(this).parent().parent();
+			console.log(mover);
+			console.log(mover.next());
+
+			$(mover).fadeOut(200).before(mover.next()).fadeIn(200);
+			$(mover.prev()).fadeOut(200).fadeIn(200);
+		});
+
+		$('.deleteRow').on('click',function(e) {
+			console.log('eliminar');
+			var parentDelete = $(this).parent().parent();
+			console.log(parentDelete);
+			parentDelete.animate({
+				opacity: 0
+			},400, function(parentDelete) {
+				console.log(parentDelete);
+				parentDelete.remove();
+			});
+		});
+	}
+});
---
+++ js/main.js
@@ -0,0 +1,105 @@
+// fondos automáticos de bing
+//http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=5&mkt=en-US
+
+// detectar favicon de un dominio
+//http://s2.googleusercontent.com/s2/favicons?domain_url=openmedicaldesigns.hol.es
+
+// items de la página
+/*var itemsAntiguos = [
+	{'tit':'Google','logo':'google.png','link':'http://google.es','count':'0'},
+	{'tit':'Youtube','logo':'youtube.png','link':'http://youtube.com','count':'0'},
+	{'tit':'Outlook','logo':'outlook.ico','link':'https://outlook.live.com/owa/?mkt=es-us&path=/mail/inbox','count':'0'},
+	{'tit':'Drive','logo':'drive.png','link':'https://drive.google.com/drive/my-drive','count':'0'},
+	{'tit':'Wikipedia','logo':'wikipedia.png','link':'https://es.wikipedia.org/wiki/Wikipedia:Portada','count':'0'},
+	{'tit':'Maps','logo':'maps.png','link':'https://www.google.es/maps?hl=es','count':'0'},
+	{'tit':'Memrise','logo':'memrise.jpg','link':'https://memrise.com','count':'0'},
+	{'tit':'Marca','logo':'marca.png','link':'https://marca.com','count':'0'},
+	{'tit':'Duolingo','logo':'duolingo.svg','link':'https://duolingo.com','count':'0'},
+	{'tit':'Gmail','logo':'gmail.ico','link':'https://mail.google.com','count':'0'},
+	{'tit':'El Mundo','logo':'elmundo.png','link':'https://elmundo.es','count':'0'},
+	{'tit':'Bing','logo':'bing.ico','link':'https://bing.com','count':'0'},
+	{'tit':'Translate','logo':'translate.png','link':'https://translate.google.com','count':'0'},
+	{'tit':'localhost','logo':'localhost.png','link':'https://localhost','count':'0'},
+	{'tit':'Spotify','logo':'spotify.jpg','link':'https://open.spotify.com','count':'0'},
+	{'tit':'Github','logo':'github.png','link':'https://github.io','count':'0'},
+	{'tit':'ytPlayer','logo':'ytPlayer.png','link':'https://sergioalvarezweb.hol.es','count':'0'},
+	{'tit':'Twitter','logo':'twitter.png','link':'https://twitter.com','count':'0'},
+	{'tit':'Atresplayer','logo':'atresplayer.png','link':'http://www.atresplayer.com/','count':'0'},
+];*/
+
+/*var userSettings = {
+	'avatar':'img/avatar.jpg',
+	'username':'Sergio',
+	'searchEngine':'google'
+};*/
+
+if (localStorage.getItem('startz-items') == null) { localStorage.setItem('startz-items',''); }
+if (localStorage.getItem('startz-settings') == null) { localStorage.setItem('startz-settings',''); }
+
+var engines = [
+	{'name':'google','search':'http://www.google.com/search','queryVar':'q'},
+	{'name':'bing','search':'https://www.bing.com/search','queryVar':'q'},
+	{'name':'yahoo','search':'https://es.search.yahoo.com/search','queryVar':'p'},
+	{'name':'DuckDuckGo','search':'https://duckduckgo.com','queryVar':'q'}
+];
+
+var items = JSON.parse(localStorage.getItem('startz-items'));
+
+$(function() {
+	// mostrar los items
+	var box = '';
+	for (var i in items) {
+		box += '<div class="col-xs-12 col-sm-6 col-md-3 col-lg-2">';
+        	box += '<a class="link" href="' + items[i]['link'] + '"><article class="totalVisitas' + items[i]['count'] + '">';
+				if (items[i]['logo'].substring(0,7) == 'https:/' || items[i]['logo'].substring(0,7) == 'http://') {
+					box += '<img src="' + items[i]['logo'] + '" />';
+				} else {
+					box += '<img src="img/' + items[i]['logo'] + '" />';
+				}
+                box += '<p>' + items[i]['tit'] + '</p>';
+            box += '</article></a>';
+        box += '</div>';
+	}
+
+	if (items.length==0) {
+		box += '<p class="noResults">Todavía no tienes enlaces disponibles</p>';
+	}
+	$('#itemContainer').html(box);
+
+	// detectar datos de configuración
+	if (localStorage.getItem('startz-settings') != null) {
+		var userSettings = JSON.parse(localStorage.getItem('startz-settings'));
+
+		$('#session').find('img').attr('src',userSettings.avatar);
+		$('#session').find('img').attr('title',userSettings.username);
+		$('#session').find('img').attr('alt',userSettings.username);
+
+		for (i in engines) {
+			if (engines[i]['name'] == userSettings.searchEngine) {
+				$('header').find('form').attr('action',engines[i].search);
+				$('#searchBox').attr('name',engines[i].queryVar);
+				$('#searchBox').attr('placeholder','buscar con ' + engines[i].name);
+			}
+		}
+	}
+
+	// botón eliminar texto cuadro de búsqueda
+	$('#deleteSearch').on('click', function() {
+		$('#searchBox').val('');
+	});
+
+	// aumenta el contador de visitas a un item
+	$('article').on('click', function() {
+		var items = JSON.parse(localStorage.getItem('startz-items'));
+		var tit = $(this).find('p').html();
+		var count = $(this).attr('class').substring(12);
+
+		for (var i in items) {
+			if (items[i]['tit']==tit) {
+				items[i]['count']++;
+			}
+		};
+
+		localStorage.setItem('startz-items',JSON.stringify(items));
+	});
+});
